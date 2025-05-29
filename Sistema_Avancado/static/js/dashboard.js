76dde06a90154442f80df86c87ce49f7d35edb6f
Ju300 <89142228+Ju300@users.noreply.github.com>
2025-05-29 19:57:48 -0300

    Update dashboard.js

--- Sistema_Avancado/static/js/dashboard.js
+++ Sistema_Avancado/static/js/dashboard.js
@@ -1,11 +1,23 @@
 function initDashboard() {
+    // Define um ouvinte (listener) para o evento 'fila_atualizada'.
+        // Toda vez que o servidor enviar esse evento, a função será executada.
+    
     const socket = io();
+    // Cria uma conexão WebSocket usando a biblioteca Socket.io.
+    // Permite comunicação em tempo real entre o servidor e o cliente (navegador).
     
     socket.on('fila_atualizada', function(data) {
         // Atualizar fila de chamados
+        // Define um ouvinte (listener) para o evento 'fila_atualizada'.
+        // Toda vez que o servidor enviar esse evento, a função será executada.
+
+        // === Atualização da fila de chamados ===
         const filaDiv = document.getElementById('fila');
+         // Seleciona o elemento HTML com id 'fila', onde serão exibidos os chamados.
+
         filaDiv.innerHTML = '';
-        
+         // Limpa o conteúdo atual da fila antes de inserir os novos dados.
+
         data.fila.forEach(chamado => {
             const div = document.createElement('div');
             div.className = `chamado prioridade-${chamado.prioridade[0]}`;
